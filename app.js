a92127f86c7e48599b2fc5482da974235ad6ad6e
jamilhassanuk <jamil@jamilhassan.co.uk.com>
2021-02-24 20:09:47 +0000

    restyling porfolio Home Page0.1

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // your code here
     setTimeout(function(){el = document.getElementById("skills");
     el.style.visibility = "visible"; }, 1000);
+
+    setTimeout(function(){ele = document.getElementById("projectResumeContainer");
+    ele.style.visibility = "visible"; }, 1300);
     
  }, false);
 
