e9c38e57cdbfe4e4a5facbce63ef1cc8c8eaa42c
jamilhassanuk <jamil@jamilhassan.co.uk.com>
2021-02-24 19:46:40 +0000

    restyling porfolio Home Page

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+/*delay skill appearing on screen */
+document.addEventListener('DOMContentLoaded', function() {
+    // your code here
+    setTimeout(function(){el = document.getElementById("skills");
+    el.style.visibility = "visible"; }, 1000);
+    
+ }, false);
+
+
+
+ 
